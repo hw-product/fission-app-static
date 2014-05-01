e37a051ee1472a707ff5db3ce9d7b4f186a76daf
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-01 14:21:52 -0700

    Include root static page routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 's(/:path)', to: 'static#display', :constraints => {:path => /.*/}
+  get '(:path)', to: 'static#display', :constraints => {:path => /(?!assets).+/} , :as => :static_page
 end
