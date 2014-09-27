5c6b18a687947a9bb0d082a7480f45fb79c5ba86
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-27 12:47:14 -0700

    Fix routing to not match assets within glob

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :static_pages
   end
-  get 'static/:product/:path', to: 'static_pages#show', :constraints => {:path => /.*/}
-  get '(:product/):path', to: 'static_pages#show', :constraints => {:path => /(?!assets).+/} , :as => :static_page
+  get 'static/:product/:path', to: 'static_pages#show', :constraints => {:path => /static.*/}
+  get '(:product/):path', to: 'static_pages#show', :constraints => lambda{|req|
+    !req.path.start_with?('/assets')
+  }
 end
