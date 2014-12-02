fc1713bd96e14218b9a35aefc28e85d19c4216d2
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-01 19:03:40 -0800

    Add route for performing lookup within static pages for non-matching routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :static_pages
   end
-  get 'static/:product/:path', to: 'static_pages#show', :constraints => {:path => /static.*/}
-  get ':path', to: 'static_pages#show', :constraints => lambda{|req|
-    puts "**************"
-    p req.path
-    !req.path.start_with?('/assets')
-  }
+  match '*path', to: 'static_pages#show', via: :all
 end
