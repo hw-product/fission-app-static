4d35ff6f4bcc224cfc0972774b754781be4b0990
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-03 10:04:30 -0800

    Add constraint on catchall match to allow assets to pass through

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :static_pages
   end
-  match '*path', to: 'static_pages#show', via: :all
+  match '*path', to: 'static_pages#show', via: :all, :constraints => lambda{|req|
+    !req.path.start_with?('/assets')
+  }
   root 'static_pages#show'
 end
