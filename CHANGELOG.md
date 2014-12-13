e828184047de7faa3a42d4db116b517cb58a67a3
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-13 07:36:19 -0800

    Updates for release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+## v0.1.2
+* Allow mix of file content and DB content
+* Fix routing rules
+
 ## v0.1.0
 * Initial release
--- lib/fission-app-static/version.rb
+++ lib/fission-app-static/version.rb
@@ -1,7 +1,5 @@
 module FissionApp
   module Static
-    class Version < Gem::Version
-    end
-    VERSION = Version.new('0.1.1')
+    VERSION = Gem::Version.new('0.1.2')
   end
 end
