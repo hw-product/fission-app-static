5987436c130cc9a45f012d92b32dcb04b8715105
Chris Roberts <code@chrisroberts.org>
2015-05-30 07:48:56 -0700

    Add navigation access

--- lib/fission-app-static/engine.rb
+++ lib/fission-app-static/engine.rb
@@ -1,6 +1,34 @@
 module FissionApp
   module Static
     class Engine < ::Rails::Engine
+
+module FissionApp
+  module Configs
+    class Engine < ::Rails::Engine
+
+      config.to_prepare do |config|
+      end
+
+      # @return [Array<Fission::Data::Models::Product>]
+      def fission_product
+        [
+          Fission::Data::Models::Product.find_by_internal_name('fission')
+        ]
+      end
+
+      # @return [Hash] account navigation
+      def fission_navigation(product, current_user)
+        if(product.internal_name == 'fission')
+          Smash.new(
+            'Admin' => Smash.new(
+              'Pages' => Rails.application.routes.url_helpers.admin_static_pages_path
+            )
+          )
+        else
+          Smash.new
+        end
+      end
+
     end
   end
 end
