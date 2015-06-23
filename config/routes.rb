1508c59b3d659af2debdd38d2a0d1a3aad4f2958
Chris Roberts <code@chrisroberts.org>
2015-06-23 16:50:43 -0700

    Provide route helper methods for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :static_pages
   end
-  match '*path', to: 'static_pages#show', via: :all, :constraints => lambda{|req|
+  match '*path', :to => 'static_pages#show', :via => :all, :constraints => lambda{|req|
     !req.path.start_with?('/assets')
   }
+  get ':prefix/:page', :to => 'static_pages#show', :as => :static_page
   root 'static_pages#show'
 end
