ef02824889e8e2d2cdd19f6ea31f5d4378b4665e
Chris Roberts <code@chrisroberts.org>
2015-06-26 08:02:23 -0700

    Add constraint on named route to prevent matching assets. Remove custom assets

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :static_pages
   end
-  get 'static-assets/(*path)(.:format)', :to => 'static_pages#deliver_asset', :as => :static_asset
   match '*path', :to => 'static_pages#show', :via => :all, :constraints => lambda{|req|
     !req.path.start_with?('/assets')
   }
-  get ':prefix/:page', :to => 'static_pages#show', :as => :static_page
+  get ':prefix/:page', :to => 'static_pages#show', :as => :static_page, :constraints => lambda{|req|
+    !req.path.start_with?('/assets')
+  }
   root 'static_pages#show'
 end
